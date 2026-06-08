7d7e1ad8a0869385931381a5a57263768530596b
Derek <derex1987@gmail.com>
2026-06-08 00:36:56 -0500

    feat: prevent player deaths with the listener permission on

--- src/main/java/io/github/derexxd/silentDeathMessages/listeners/PlayerDeathListener.java
+++ src/main/java/io/github/derexxd/silentDeathMessages/listeners/PlayerDeathListener.java
@@ -1,4 +1,20 @@
 package io.github.derexxd.silentDeathMessages.listeners;
 
-public class PlayerDeathListener {
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class PlayerDeathListener implements Listener {
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+
+        if (player.hasPermission("silentdeathmessages.silent")) {
+            event.deathMessage(null);
+            event.setDeathMessage(null);
+            // compat. idk. im thirsty i need waterrr
+        }
+    }
 }
